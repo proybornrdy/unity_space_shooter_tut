6307ca5ca8486fd41060ea7f4f1eb11c994257a0
Seung gyu Lee <seungq92@gmail.com>
2018-05-28 21:51:32 -0400

    player now moveable

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private  Rigidbody rb;
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate (){
+        float moveHorizon = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement =  new Vector3(moveHorizon, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+        
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
+        rb.rotation= Quaternion.Euler(0.0f,0.0f, rb.velocity.x * -tilt);
+
+
+    }
 }
