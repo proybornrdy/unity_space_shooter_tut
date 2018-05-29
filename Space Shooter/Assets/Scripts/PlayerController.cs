bb5eec4b1e93f6bfe6a1d29542a48932112edb88
Seung gyu Lee <seungq92@gmail.com>
2018-05-29 19:55:52 -0400

    asteroid + shots

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -14,9 +14,24 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Boundary boundary;
     public float tilt;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate;
+    private float nextFire;
 
     void Start(){
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1")&&Time.time > nextFire){
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+        }
+
     }
 
     void FixedUpdate (){
